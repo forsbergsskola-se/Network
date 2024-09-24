4027cef7b22566314617e1cd5cd08794031d87cb
Jonatan Luna <jonatan.luna@student.forsbergsskola.se>
2024-09-24 16:10:32 +0200

    Comunication with server

---
+++ TimeClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Connecting to server...");
+var client = new TcpClient();
+client.Connect("192.168.1.57", 44444);
+Console.WriteLine("Server accepted: " + client.Client.RemoteEndPoint);
+
+// Create an array in which we can store the received bytes
+var buffer = new byte[1000]; 
+// Read as many bytes as we can from the stream. Maximum 1000. The first byte of the array
+// can be used.
+var bytesReceived = client.GetStream().Read(buffer, 0, buffer.Length);
+// Convert the bytes to readable text using ASCCI format
+var currentTime = Encoding.ASCII.GetString(buffer);
+
+Console.WriteLine(currentTime);
---
+++ TimeServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Starting up server...");
+var listener = new TcpListener(IPAddress.Any, 44444);
+listener.Start();
+Console.WriteLine("Listening for incoming connections on " + listener.LocalEndpoint + "...");
+
+while (true)
+{
+    // Accept a new incoming connection. Wait for it.
+    var client = listener.AcceptTcpClient(); // Blocking
+    Console.WriteLine(("Client accepted: " + client.Client.RemoteEndPoint));
+// The text we want to send: 
+    string currentTime = "Hola Putit@. The current time is: " + DateTime. Now.ToString("f");
+// Encode the text using ASCII:
+// The server commands the protocol:
+// I send one ASSCII-Encoded String and then close the connection.
+    byte[] bytes = Encoding.ASCII.GetBytes(currentTime);
+// Send the bytes over the clients stream:
+    client.GetStream().Write(bytes);
+    client.Close();
+    Console.WriteLine("Closing server...");
+}
